769c15002f43ea468207aa9a84140edaa6ff2639
campa1987 <48693868+campa1987@users.noreply.github.com>
2022-08-13 07:55:47 -0300

    Create jobscript.sh

---
+++ jobscript.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+echo "Bienvenido/a al planeta $planeta"
+if [ "$agente" = "true" ]
+then
+	echo "Prepárese para la misión agente X, que la fuerza te acompañe."
+else
+	echo "Disfruta el planeta humano/a $nombre, trata de no morir."
+fi
+echo "..."
+sleep 10
+echo "¡Suerte! La necesitarás."
+Footer
+© 2022 GitHub, Inc.
+Footer navigation
+Terms
+Privacy
